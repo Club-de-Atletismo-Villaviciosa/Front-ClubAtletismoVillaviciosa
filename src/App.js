45bc808e423375e2fc1b0ce165dbe8c80b99da4d
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-04-25 12:53:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-              <iframe src="https://calendar.google.com/calendar/embed?src=f016b45aaa5d8443f53ead02a13bd1fcccfc738fab8a8d186f4afdac2fa8b14e%40group.calendar.google.com&ctz=UTC" style={{border: 0}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
     </div>
   );
