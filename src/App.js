b616b48113f1c08bbc65544ea87225138cb66b43
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-04-26 14:16:58 +0200

    calendar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Calendar from './Components/Calendar/Calendar';
 
 function App() {
   return (
     <div className="App">
+      <Calendar />
     </div>
   );
 }
---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Calendar.css"
+
+function Calendar() {
+  return (
+    <div>
+      <iframe src="https://www.google.com/calendar/embed?color=%23ffad46&amp;deb=-&amp;embed_style=WyJhdDplbWI6c3QiLCIjZTBlMGUwIiwiI2VkZWRlZCIsIiM0MTg0ZjMiLCJyb2JvdG8iLCIjNjM2MzYzIiw1MDAsIiNmZmYiXQo&amp;eopt=2&amp;mode=MONTH&amp;showCalendars=1&amp;showDate=0&amp;showNav=0&amp;showPrint=0&amp;showTitle=0&amp;showTz=1&amp;src=ZjAxNmI0NWFhYTVkODQ0M2Y1M2VhZDAyYTEzYmQxZmNjY2ZjNzM4ZmFiOGE4ZDE4NmY0YWZkYWMyZmE4YjE0ZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"  width="800" height="600" frameborder="0" aria-label="Calendar, Competiciones" allowfullscreen=""></iframe>
+    </div>
+  )
+}
+
+export default Calendar
