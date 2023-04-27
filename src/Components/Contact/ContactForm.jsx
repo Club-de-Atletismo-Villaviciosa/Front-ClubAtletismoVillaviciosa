2ef344249657a420d25dd87d5b51d44073108cd1
JavierBP <fjavierbresperez@yahoo.es>
2023-04-27 23:38:25 +0200

    ContactForm modified, input with border-radius

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -13,30 +13,31 @@ const ContactForm = () => {
     }
 
     return (
-        <div>
+        <div className='container'>
             <div>
-                <h1 className='headerForm'>CLUB ATLETISMO VILLAVICIOSA</h1>
+                <h1>CLUB ATLETISMO VILLAVICIOSA</h1>
             </div>
             <form className='contacform' onSubmit={handleSubmit}>
             <h1>CONTACTO</h1>
-                <div className='nameForm'>
-                    <label for="name">Nombre:*</label>
-                    <input type="text" maxLength="30" placeholder='Inserta tu nombre' autoFocus required
+                <div className='form-item'>
+                    <label for="name">Nombre:*</label><br />
+                    <input type="text" name="name" id='name' maxLength="30" placeholder='Inserta tu nombre' autoFocus required
                         onChange={(event) => { setName(event.target.value) }} />
                 </div>
-                <div className='emailForm'>
-                    <label for='email'>Email:*</label>
-                    <input type="email" pattern='^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
-                        name='email' placeholder='Inserta tu correo electrónico' required
+                <div className='form-item'>
+                    <label for='email'>Email:*</label><br />
+                    <input type="email" name="email" id='email' pattern='^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+                        placeholder='Inserta tu correo electrónico' required
                         onChange={(event) => { setEmail(event.target.value) }} />
                 </div>
-                <div className='textForm'>
-                    <label>Consulta:*</label>
-                    <textarea rows="6" name="comment" required onChange={(event) => { setTextArea(event.target.value) }}>
+                <div className='form-item'>
+                    <label>Consulta:*</label><br /> 
+                    <textarea name="message" rows="10" cols="30" placeholder='Mensaje' required 
+                    onChange={(event) => { setTextArea(event.target.value) }}>
                     </textarea>
                 </div>
-                <div>
-                    <input type="submit" value="Enviar consulta" />
+                <div className='form-item'>
+                    <input className='inputForm' type="submit" name='submit' id='submit' value="Enviar consulta" />
                 </div>
 
                 <div>
@@ -49,7 +50,7 @@ const ContactForm = () => {
                 <div className='map'>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2299.2608046790697!2d-5.432351342090198!3d43.48549702484571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd366d0a47d91b0d%3A0xc37e8fd7ffa6589c!2spista%20de%20atletismo%20municipal!5e1!3m2!1sca!2ses!4v1682414921244!5m2!1sca!2ses"
-                        title="mapGoogle" width="450" height="260" allowfullscreen="" loading="lazy"
+                        title="mapGoogle" width="670" height="320" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import El_Gaitero_logo from '../../assets/img/El_Gaitero_logo.svg';
-import Escudo_Villaviciosa from '../../assets/img/Escudo_Villaviciosa.png';
 import './Footer.css';
 
 const Footer = () => {
+    
     return (
         <div>
             <footer>
                 <p>Patrocinado por:</p>
                 <div className='logos'>
-                    <img className='gaitero' src={El_Gaitero_logo} alt="Sidra El Gaitero" width="150" />
-                    <img src={Escudo_Villaviciosa} alt="Escudo Villaviciosa" width="150" />                  
+                    <img className='gaitero' src={El_Gaitero_logo} alt="Sidra El Gaitero" width="125" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/79/Escudo_de_Villaviciosa.svg" 
+                    alt="Escudo Villaviciosa" width="125" />                  
                 </div>
             </footer>
         </div>
