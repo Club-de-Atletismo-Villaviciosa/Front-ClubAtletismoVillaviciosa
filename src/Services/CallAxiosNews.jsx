c7b1247d3c139f75cf398c1e046c9b3bc5a2955b
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-04-28 09:20:36 +0200

    changed url to UrlRequest

--- src/Services/CallAxiosNews.jsx
+++ src/Services/CallAxiosNews.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const CallAxiosNews = () => {
 
-const url = "http://localhost:8080/api/v1/news"
+
 
 const getNews = async () => {
     const res = await axios.get(`${url}`);
