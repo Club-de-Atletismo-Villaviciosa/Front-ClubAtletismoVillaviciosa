2da7a698d98648b9a0689b5f4f140192c0d0a989
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-05-05 12:25:12 +0200

    fixed base64 only post

--- src/Components/FormMain/FormMain.jsx
+++ src/Components/FormMain/FormMain.jsx
@@ -11,6 +11,7 @@ function FormMain(){
     const url = "http://localhost:8080/api/v1/news"
     const State = useLocation().state 
     let [data, setData] = useState(State ? State : {title: "", url: "", news: ""})
+    const [base64, setBase64] = useState("");
   
 
 function handleSubmit(event) {
@@ -25,13 +26,13 @@ function handleChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        let temp_data = { ...data };
+        let temp_data = { ...data};
         if (value !== undefined) {
           temp_data[name] = value;
         }
         setData(temp_data);
       }
-    console.info(data)
+    console.info(data.url)
 
     return(
         <form className="formMain" onSubmit={handleSubmit} method="post">
@@ -42,14 +43,17 @@ function handleChange(event) {
             <div className="formMain-image">
                 <label>Imagen:</label>
                 <div className="formMain-image-inputAndButton">
-                    <input type="url" name="url" onChange={handleChange} defaultValue={State ? State.url: ""} autoComplete="off" placeholder="Enlace de Imgur" required pattern="https?://.+" />
-                    {/* <InputPhoto handleChange ={handleChange}/> */}
+                    {/* <input type="url" name="url" onChange={handleChange} defaultValue={State ? State.url: ""} autoComplete="off" placeholder="Enlace de Imgur" required pattern="https?://.+" /> */}
+                    
+                    <textarea name='url' defaultValue={State ? State.url : base64} required className="form-control" placeholder="Añade una foto"  onChange= {handleChange}
+                                id="photo"/>
+                    <InputPhoto setBase64 ={setBase64} setData={setData}/> 
                     <p>(Asegurate que la foto esté centrada)</p>
                 </div>
             </div>
                 <div className="formMain-activity">
                 <label>Actividad:</label>
-                <textarea rows="15" type="text" name="news"  defaultValue={State ? State.description : ""} onChange={handleChange} id="" required/>
+                <textarea rows="10" type="text" name="news"  defaultValue={State ? State.description : ""} onChange={handleChange} id="" required/>
             </div>
             <div className="formMain-postButton">
                 <button><span>Publicar</span></button>
