50a069a45123b9a6f0b9223db79ecda3a28b1068
JavierBP <fjavierbresperez@yahoo.es>
2023-05-11 21:59:38 +0200

    create media queries Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,16 +6,25 @@ const Footer = () => {
 
     return (
         <div>
-            <footer>
-                <div className='footer-p'>
-                    <p>Patrocinado por:</p>
+        <footer>
+        <p className='phrase'>Patrocinado por:</p>
+        
+            <div className='footer'>           
+                <div className='footer-footerPartners'>
+                    <div className='footer-gaitero'>
+                        <img className='gaitero' src={El_Gaitero_logo} alt="Sidra El Gaitero"  />
+                        <p>Sidra El Gaitero</p>
+                    </div>
+                    <div className='footer-ayto'>
+                        <img className='ayto' src="https://upload.wikimedia.org/wikipedia/commons/7/79/Escudo_de_Villaviciosa.svg"
+                        alt="Escudo Villaviciosa"  />
+                        <p>Ayuntamiento de Villaviciosa</p>
+                    </div>
                 </div>
-                <div>
-                    <img className='gaitero' src={El_Gaitero_logo} alt="Sidra El Gaitero" width="100" />                    
                 </div>
-                
-            </footer>
-        </div>
+
+        </footer>
+    </div>
     )
 }
 
