7c8d28937b86bca3448ef7e553080776460b4688
JavierBP <fjavierbresperez@yahoo.es>
2023-05-16 21:30:24 +0200

    finished to add more things

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
         <div>
             <footer>
                 <p className='phrase'>Patrocinado por:</p>
-
                 <div className='footer'>
                     <div className='footer-footerPartners'>
                         <div className='footer-gaitero'>
@@ -22,20 +21,17 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div>
-                    <p>Correo electrónico</p>
-                </div>
-                <div>
-                    <div>
-                    © 2023 Copyrigth
+                <div className='footer-details'>
+                    <div className='footer-email'>
+                        <p>elgaiteroatletismo@gmail.com</p>
                     </div>
-                    <div>
-                        Factoría F5 (Christian Carbajo, Salah Righab, Aurino Llera, Javier Bres)
+                    <div className='footer-coders'>
+                        <p><span className='f5'>factoría F5</span> (Christian Carbajo, Salah Raghib, Aurino Llera, Javier Bres)</p>
+                    </div>
+                    <div className='footer-copyright'>
+                        <p>© 2023 Copyrigth</p>
                     </div>
                 </div>
-
-                <p>Cookies</p>
-
             </footer>
         </div>
     )
