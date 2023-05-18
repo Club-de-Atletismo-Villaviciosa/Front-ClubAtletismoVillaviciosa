46049b2be701ef839b37da580a545c827d484827
JavierBP <fjavierbresperez@yahoo.es>
2023-05-18 14:48:21 +0200

    re-update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { IoIosArrowDown } from "react-icons/io"
@@ -10,6 +8,7 @@ import { FaBars } from "react-icons/fa";
 import { BiLogOut, BiLogIn } from "react-icons/bi";
 import { VscAccount } from "react-icons/vsc";
 
+
 function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
     let [mode, setMode] = useState("close")
@@ -17,7 +16,7 @@ function Navbar() {
 
 
     function handleDropdownClick(e) {
-        mode == "close"? setMode("open") : setMode("close")        
+        mode == "close" ? setMode("open") : setMode("close")
     }
 
     const handleShowNavbar = () => {
@@ -25,7 +24,7 @@ function Navbar() {
     }
 
     return (
-        <nav  className='navbar'>
+        <nav className='navbar'>
             <div className='navbar-icons'>
                 <button className='navbar-btn' onClick={handleShowNavbar}>
                     <FaBars />
@@ -35,19 +34,20 @@ function Navbar() {
                 </div>
             </div>
             <div className={`navbar-elements ${isNavExpanded && 'active'}`}>
-            <ul className='navbar-ul'>
-                <Link to={"/"}><li>Inicio</li></Link>
-                <Link to={"/CalendarPage"}><li>Competiciones</li></Link>
-                <li className="click" onClick={handleDropdownClick}>Info. del club<IoIosArrowDown /></li>
-                <ul data-testid="navbar-dropdown" className={`navbar-ul-dropdown ${mode}`} >
-                    <Link to={"/InfoPage"}><li>Historia</li></Link>
-                    <Link to={"/InfoPage"}><li>Palmarés</li></Link>
-                    <Link to={"/InfoPage"}><li>Perfiles de los atletas</li></Link>
+                <ul className='navbar-ul'>
+                    <Link to={"/"}><li>Inicio</li></Link>
+                    <Link to={"/"}><li>Competiciones</li></Link>
+                    <li className="click" onClick={handleDropdownClick}>Info. del club<IoIosArrowDown /></li>
+                    <ul data-testid="navbar-dropdown" className={`navbar-ul-dropdown ${mode}`} >
+                        <Link to={"/InfoPage"}><li>Historia</li></Link>
+                        <Link to={"/InfoPage"}><li>Palmarés</li></Link>
+                        <Link to={"/InfoPage"}><li>Perfiles de los atletas</li></Link>
+                    </ul>
+                    <Link className='navbar-contacto' to="/ContactPage"><li>Contacto</li></Link>
                 </ul>
-                <Link className='navbar-contacto' to="/ContactPage"><li>Contacto</li></Link>
-            </ul>
             </div>
-            <div className='navbar-bar'></div>
+            <div className='navbar-bar'>                
+            </div>
             <div className='navbar-btnLog'>
                 <Link to ={"/loginPage"}><VscAccount color='white' className='navbar-btnLogOut' /></Link>
                 {/* <BiLogOut className='navbar-btnLogOut' /> */}
@@ -57,4 +57,3 @@ function Navbar() {
 }
 
 export default Navbar
->>>>>>> ce6c9db700139e7716015be98c9c7ab896222c83
