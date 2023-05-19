861aa18787e0109c6b136d6f897107cf1e064d95
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-05-19 14:48:18 +0200

    a

--- src/Services/ApiDeleteService.jsx
+++ src/Services/ApiDeleteService.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
 
 async function ApiDeleteService(url, id) {
+    let token = Cookies.get("user")
+
     try {
-        await axios.delete(url + `/delete/${id}`)
+        await axios.delete(url + `/delete/${id}`, {headers: {Authorization: `Bearer ${JSON.parse(token)}`}})
     } catch (error) {
         console.log(error)
     }
--- src/Services/ApiPutService.jsx
+++ src/Services/ApiPutService.jsx
@@ -1,9 +1,11 @@
 import axios from "axios"
+import Cookies from "js-cookie"
 
 
 async function ApiPutService(url, item, id) {
+  let token = Cookies.get("user")
   try {
-    let res = await axios.put(url + '/update/' + id, item)
+    let res = await axios.put(url + '/update/' + id, item, {headers: {Authorization: `Bearer ${JSON.parse(token)}`}})
     console.log(res)
   } catch (error) {
     console.log(error)
