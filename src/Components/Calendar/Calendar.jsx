ba1d3c37d1c4e2b385fe267be677e5e25ee8fd26
JavierBP <fjavierbresperez@yahoo.es>
2023-05-21 14:47:10 +0200

    change color calendar

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -4,13 +4,10 @@ import "./Calendar.css"
 function Calendar() {
   return (
     <div className='calendar'>
-      <h2>Calendario de Competiciones</h2>
-      
-      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FMadrid&title=El%20Gaitero%20Atletismo&src=bWFnYXR6ZW0ucnVzY0BnbWFpbC5jb20&src=Y2Euc3BhaW4ub2ZmaWNpYWwjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043"
+      <h2>Calendario de Competiciones</h2>      
+
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%238E24AA&ctz=Europe%2FMadrid&title=El%20Gaitero%20Atletismo&src=bWFnYXR6ZW0ucnVzY0BnbWFpbC5jb20&src=Y2Euc3BhaW4ub2ZmaWNpYWwjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043"
         width="800" height="600" frameborder="0" scrolling="no"></iframe>
-      
-      {/* <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%238E24AA&ctz=Europe%2FMadrid&title=El%20Gaitero%20Atletismo&src=dmlsbGFyZXMubGFlaXJhQGdtYWlsLmNvbQ&src=Y2Euc3BhaW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%237986CB"
-        width="800" height="500" frameborder="0" scrolling="no"></iframe> */}
 
     </div>
   )
