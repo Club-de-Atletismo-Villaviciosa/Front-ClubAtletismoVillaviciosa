edc91e30bd81b47a43190701f92cf61b0b9276ca
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-05-22 14:54:15 +0200

    worked in date function

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -3,12 +3,15 @@ import { Navigate, useNavigate } from 'react-router-dom'
 import pencil from "../../assets/img/pencil.png"
 import trash from "../../assets/img/trash.png"
 import ApiDeleteService from "../../Services/ApiDeleteService"
+import ApiGetByIdService from "../../Services/ApiGetByIdService"
 import Confirmation from "../Confirmation/Confirmation"
 import { useState } from "react"
 import { useContext } from "react"
 import { AuthContext } from "../../Services/AuthContext"
 
 function NewsCard({ id, url, title, news, customClass }) {
+
+    const [data, setData] = useState({});
 
     const urlGeneral = "http://localhost:8080/api/v1/news"
     let navigate = useNavigate()
@@ -31,6 +34,13 @@ function NewsCard({ id, url, title, news, customClass }) {
         setConfirmation(true)
     }
 
+    function getById(){
+    ApiGetByIdService(urlGeneral,id)
+    .then((data) => setData(data))
+    .catch((error) => console.error(error));
+}
+console.info(data)
+
     return (
         <div className={`${customClass}`}>
             {confirmation && <Confirmation type={"delete"} handleAction={handleDelete} handleClose={handleClose} />}
@@ -39,6 +49,7 @@ function NewsCard({ id, url, title, news, customClass }) {
                 <div className="newsCard-titlePencilTrash">
                     <h2  onClick={() => { navigate("/NewsPage", { state: { id, title, url, news } }) }} alt="Título de la noticia">{title}</h2>
                     {isLogged && (<><img onClick={() => { navigate("/NewsPage", { state: { id, title, url, news, edit: true} }) }} src={pencil} />
+                    <p>{data.date}</p>
                     <img onClick={handleClickSubmit} src={trash} /></>)}
                 </div>
                 <p className="newsCard-bodyNews" alt="Texto de la noticia" >{news ? news.substring(0, maxLength) : ""}<span className="newsCard-span" onClick={() => { navigate("/NewsPage", { state: { id, title, url, news } }) }}>... Leer mas</span></p>
